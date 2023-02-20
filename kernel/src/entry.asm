6bcaab7269c352028674b64fe98b091806b38f29
Rift <2960252562@qq.com>
2023-02-20 17:48:01 +0800

    create a 64KB stack for kernel

--- kernel/src/entry.asm
+++ kernel/src/entry.asm
@@ -1,4 +1,12 @@
 	.section .text.entry
 	.global _start
 _start:
-	li x1,100
+    la sp, boot_stack_top  #设置sp指向栈底(sp是指向栈顶的指针，但此时栈空)
+	call rust_main
+
+    .section .bss.stack
+    .globl boot_stack_lower_bound
+boot_stack_lower_bound:
+    .space 4096 * 16  #64KB stack
+	.globl boot_stack_top
+boot_stack_top:
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -7,4 +7,8 @@ use core::arch::global_asm;
 
 global_asm!(include_str!("entry.asm"));
 
+#[no_mangle]
+pub fn rust_main() -> ! {
+    loop {}
+}
     // println!("Hello, world!");
