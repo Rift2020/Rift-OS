9e00e8d16a6852eaa43057395d7fc156f6a5e6c8
Rift <2960252562@qq.com>
2023-02-20 20:56:16 +0800

    add clear_bss

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -9,6 +9,18 @@ global_asm!(include_str!("entry.asm"));
 
 #[no_mangle]
 pub fn rust_main() -> ! {
+    clear_bss();
     loop {}
 }
-    // println!("Hello, world!");
+
+fn clear_bss() {
+    extern "C" {
+        static sbss: u64;
+        static ebss: u64;
+    }
+    unsafe {
+        (sbss..ebss).for_each(|a| {
+            (a as *mut u8).write_volatile(0)
+        });
+    }
+}
