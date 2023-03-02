4f9706b503614c4fda862e04c97edb1be46ece60
Rift <2960252562@qq.com>
2023-03-02 18:45:15 +0800

    add simple memory-mapping in entry.asm
    so that kernel will boot on virtual memory space

--- kernel/src/entry.asm
+++ kernel/src/entry.asm
@@ -1,12 +1,34 @@
-	.section .text.entry
-	.global _start
+    .section .text.entry
+    .globl _start
 _start:
-    la sp, boot_stack_top  #设置sp指向栈底(sp是指向栈顶的指针，但此时栈空)
-	call rust_main
+    lui     t0, %hi(boot_page_table_sv39)
+    li      t1, 0xffffffffc0000000 - 0x80000000
+    sub     t0, t0, t1
+    srli    t0, t0, 12
+    li      t1, 8 << 60
+    or      t0, t0, t1
+    csrw    satp, t0
+    sfence.vma
+
+    lui sp, %hi(bootstacktop)
+
+    lui t0, %hi(rust_main)
+    addi t0, t0, %lo(rust_main)
+    jr t0
 
     .section .bss.stack
-    .globl boot_stack_lower_bound
+    .align 12
+    .global bootstack
 boot_stack_lower_bound:
-    .space 4096 * 16  #64KB stack
-	.globl boot_stack_top
-boot_stack_top:
+    .space 4096 * 16 #64KB stack
+    .global bootstacktop
+bootstacktop:
+
+    .section .data
+    .align 12   # page align
+boot_page_table_sv39:
+    # 0xffffffff_c0000000 -> 0x80000000 (1G)
+    .zero 8 * 2
+	.quad (0x80000 << 10) | 0xcf # VRWXAD
+	.zero 8 * 508
+    .quad (0x80000 << 10) | 0xcf # VRWXAD
